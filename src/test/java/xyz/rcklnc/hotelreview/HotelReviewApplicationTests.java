a594f12b7d5b7a8f0dedc70edca754158f9b49a9
Rick Lancee <rick@lifely.nl>
2020-12-16 21:59:02 +0100

    initial project initialization
    Spring settings https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.4.1.RELEASE&packaging=jar&jvmVersion=11&groupId=xyz.rcklnc&artifactId=hotelreview&name=Hotel%20Review&description=An%20application%20that%20reviews%20hotels&packageName=xyz.rcklnc.hotelreview&dependencies=lombok,web,data-jpa

---
+++ src/test/java/xyz/rcklnc/hotelreview/HotelReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.rcklnc.hotelreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
