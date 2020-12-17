3d91c60c773331421d5932653e5fbd08833edea0
Rick Lancee <rick@lifely.nl>
2020-12-17 19:56:37 +0100

    rename classes

--- src/test/java/xyz/rcklnc/hotelreview/domain/HotelIdTest.java
+++ src/test/java/xyz/rcklnc/hotelreview/domain/HotelIdTest.java
@@ -2,32 +2,32 @@ package xyz.rcklnc.hotelreview.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class HotelIdTest {
 
     @Test
-    public void twoHotelsWithTheSameValuesShouldBeEqual() {
-        Hotel hotel = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
-        Hotel hotel2 = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
+    public void twoIdsWithTheSameValueShouldBeEqual() {
+        HotelId hotelID1 = HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936");
+        HotelId hotelID2 = HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936");
 
-        assertEquals(hotel, hotel2);
+        assertEquals(hotelID1, hotelID2);
     }
 
     @Test
-    public void twoHotelsWithDifferentIdsShouldNotBeEqual() {
-        Hotel hotel = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
-        Hotel hotel2 = Hotel.of(HotelId.from("bb4b3ccd-ec41-4734-a74a-588b9fd51cd2"), "name", "d", "a");
+    public void twoIdsWithTheDifferentValuesShouldNotBeEqual() {
+        HotelId hotelID1 = HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936");
+        HotelId hotelID2 = HotelId.from("bb4b3ccd-ec41-4734-a74a-588b9fd51cd2");
 
-        assertNotEquals(hotel, hotel2);
+        assertNotEquals(hotelID1, hotelID2);
     }
 
     @Test
-    public void twoHotelsWithDifferentFieldsButSameIdsShouldStillBeEqual() {
-        Hotel hotel = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
-        Hotel hotel2 = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name2", "d2", "a2");
+    public void creatingANewIdShouldCreateARandomId() {
+        HotelId hotelId1 = HotelId.create();
+        HotelId hotelId2 = HotelId.create();
 
-        assertEquals(hotel, hotel2);
+        assertNotEquals(hotelId1, hotelId2);
     }
-
 }
--- src/test/java/xyz/rcklnc/hotelreview/domain/HotelTest.java
+++ src/test/java/xyz/rcklnc/hotelreview/domain/HotelTest.java
@@ -2,32 +2,32 @@ package xyz.rcklnc.hotelreview.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HotelTest {
 
     @Test
-    public void twoIdsWithTheSameValueShouldBeEqual() {
-        HotelId hotelID1 = HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936");
-        HotelId hotelID2 = HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936");
+    public void twoHotelsWithTheSameValuesShouldBeEqual() {
+        Hotel hotel = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
+        Hotel hotel2 = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
 
-        assertEquals(hotelID1, hotelID2);
+        assertEquals(hotel, hotel2);
     }
 
     @Test
-    public void twoIdsWithTheDifferentValuesShouldNotBeEqual() {
-        HotelId hotelID1 = HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936");
-        HotelId hotelID2 = HotelId.from("bb4b3ccd-ec41-4734-a74a-588b9fd51cd2");
+    public void twoHotelsWithDifferentIdsShouldNotBeEqual() {
+        Hotel hotel = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
+        Hotel hotel2 = Hotel.of(HotelId.from("bb4b3ccd-ec41-4734-a74a-588b9fd51cd2"), "name", "d", "a");
 
-        assertNotEquals(hotelID1, hotelID2);
+        assertNotEquals(hotel, hotel2);
     }
 
     @Test
-    public void creatingANewIdShouldCreateARandomId() {
-        HotelId hotelId1 = HotelId.create();
-        HotelId hotelId2 = HotelId.create();
+    public void twoHotelsWithDifferentFieldsButSameIdsShouldStillBeEqual() {
+        Hotel hotel = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name", "d", "a");
+        Hotel hotel2 = Hotel.of(HotelId.from("1d28320f-c9ff-4ec6-9744-1f4ae91cf936"), "name2", "d2", "a2");
 
-        assertNotEquals(hotelId1, hotelId2);
+        assertEquals(hotel, hotel2);
     }
+
 }
